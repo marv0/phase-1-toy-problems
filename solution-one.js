b625f6e601e4c975f6d8c07700a945936965ecdc
Marvin Ogot <marvin.ogot@gmail.com>
2023-10-08 22:43:59 +0300

    Adding comments to my solution-one script

--- solution-one.js
+++ solution-one.js
@@ -1,14 +1,15 @@
-//Challenge 1: Student Grade Generator (Toy Problem)
-
+//Create gradeGenerator Function
 function gradeGenerator(marks){
 
-// Check if the input is within the range [0, 100]
+// Check if the marks are within the range of 0 - 100 with if else statement block
 if (marks < 0 || marks > 100) {
+    //Return feedback if marks are not within range
     console.log("Student marks must be between 0 and 100.");
     } else {
+    // define grade variable
     let grade;
 
-    // Determine the grade based on the marks
+    // Determine the grade based on the marks and assign the grade to grade variable
     if (marks > 79) {
         grade = "A";
     } else if (marks >= 60) {
