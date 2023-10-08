c726e66dc0f71019111818152e123bcb00e9055b
Marvin Ogot <marvin.ogot@gmail.com>
2023-10-08 21:57:13 +0300

    Solved the second js problem

---
+++ solution-two.js
@@ -0,0 +1,21 @@
+//Challenge 2: Speed Detector
+function speedDetector(speed){
+    
+    // Check if the speed is within the speed limit of 70
+    if (speed < 70) {
+        console.log("Ok.");
+        } 
+        else {
+            let demeritPoints = 0;
+            demeritPoints = Math.floor((speed - 70) / 5);
+
+            if (demeritPoints >= 12) {
+              console.log("License suspended");
+            } else {
+              console.log("Points: " + demeritPoints);
+            }
+          }
+    }
+    
+    // Call the function to start the grading process
+    speedDetector(85);
